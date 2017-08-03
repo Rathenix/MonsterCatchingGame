11cf060e94afa5f1ce1d983a75c1ae58c9aef501
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-08-02 21:32:14 -0700

    Player team now populating in text list on map

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -1,12 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiController : MonoBehaviour {
 
+    public GameController Game;
     public GoogleMap mainMap;
     public GameObject player;
+    public GameObject TeamMenu;
     public MonsterInteractionsMenu monsterInteractionsMenu;
+
+    private void Start()
+    {
+        Game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        ShowTeamText();
+    }
+
+    public void ShowTeamText()
+    {
+        var teamText = Instantiate(TeamMenu, new Vector3(-4.5f, .4f), new Quaternion(), transform.GetComponentInParent<Canvas>().transform).GetComponent<Text>();
+        teamText.text = Game.PlayerName + "'s Team\n";
+        foreach (var mon in Game.Team)
+        {
+            teamText.text += "\n" + mon.Name + "\n";
+            teamText.text += "HP: " + mon.CurrentHp + "/" + mon.MaxHp + "\n";
+            teamText.text += "Attack: " + mon.Attack + "\n";
+            teamText.text += "Defense: " + mon.Defense + "\n";
+            teamText.text += "Speed: " + mon.Speed + "\n";
+        }
+    }
 
     public MonsterInteractionsMenu ShowMonsterInteractionsMenu(GameObject target)
     {
