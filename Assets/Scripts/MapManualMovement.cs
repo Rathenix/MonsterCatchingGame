200174e202f12c7ee008472b97d572a3de093581
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-09-13 15:05:18 -0700

    New Map Asset, new Player sprite, map scene wip
    Using a new map asset that seems better. It has a built-in way to tie
    real world lat/lon to unity space. Nothing spawns anymore on the map and
    moving is different. Started tying to device so drag to move but it
    makes the map freak out. Need to fix it for 2D, I think. Changed aspect
    ratio of game to iPhone. Drastically changed the scale of th map scene
    to try and make the map work. Added a bunch of prefabs of old map
    objects.

---
+++ Assets/Scripts/MapManualMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManualMovement : MonoBehaviour {
+    CharacterController controller;
+    Vector2 moveDirection = Vector2.zero;
+    public float Speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnTouchDetachFromDeviceAndMoveByDragging();
+    }
+
+    void OnTouchDetachFromDeviceAndMoveByDragging()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            GameManager.Instance.playerStatus = GameManager.PlayerStatus.FreeFromDevice;
+            controller.Move(new Vector3(-touchDeltaPosition.x * Speed, -touchDeltaPosition.y * Speed, 0));
+            Debug.Log("Moving: " + touchDeltaPosition * Speed);
+        }
+    }
+}
