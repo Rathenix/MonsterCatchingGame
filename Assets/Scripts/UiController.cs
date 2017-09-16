9682a772cee4deffd4addac525a5a85230d53b8b
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-09-15 17:27:58 -0700

    Added team menu
    Added a menu button that shows your team with fancy healthbars and stuff

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UiController : MonoBehaviour {
 
-    public GoogleMap mainMap;
-    public GameObject player;
-    public GameObject TeamMenu;
+    public GameObject hamburgerMenu;
     public MonsterInteractionsMenu monsterInteractionsMenu;
+    public GameObject TeamEntry;
 
     private void Start()
     {
-        ShowTeamText();
-        monsterInteractionsMenu = FindObjectOfType<MonsterInteractionsMenu>();
         monsterInteractionsMenu.gameObject.SetActive(false);
+        LoadHambugerAndSetTeamText();
     }
 
     private void Update()
@@ -26,18 +26,38 @@ public class UiController : MonoBehaviour {
         }
     }
 
-    public void ShowTeamText()
+    public void LoadHambugerAndSetTeamText()
     {
-        var teamText = Instantiate(TeamMenu, new Vector3(-4.5f, .4f), new Quaternion(), transform.GetComponentInParent<Canvas>().transform).GetComponent<Text>();
-        teamText.text = GameController.PlayerName + "'s Team\n";
-        foreach (var mon in GameController.Team)
+        var teamPane = hamburgerMenu.transform.FindChild("TeamPane");
+        //var teamText = teamPane.GetComponentInChildren<TextMeshProUGUI>();
+        //teamText.text = GameController.PlayerName + "'s Team\n";
+        //foreach (var mon in GameController.Team)
+        //{
+        //    teamText.text += "\n" + mon.Name + "\n";
+        //    teamText.text += "HP: " + mon.CurrentHp + "/" + mon.MaxHp + "\n";
+        //    teamText.text += "Attack: " + mon.Attack + "\n";
+        //    teamText.text += "Defense: " + mon.Defense + "\n";
+        //    teamText.text += "Speed: " + mon.Speed + "\n";
+        //}
+        for (var mon = 0; mon < GameController.Team.Count; mon++)
         {
-            teamText.text += "\n" + mon.Name + "\n";
-            teamText.text += "HP: " + mon.CurrentHp + "/" + mon.MaxHp + "\n";
-            teamText.text += "Attack: " + mon.Attack + "\n";
-            teamText.text += "Defense: " + mon.Defense + "\n";
-            teamText.text += "Speed: " + mon.Speed + "\n";
+            var monster = GameController.Team[mon];
+            var teamPadding = 35f * mon;
+            var entry = Instantiate(TeamEntry, teamPane);
+            entry.transform.localPosition = new Vector3(-3, 115 - teamPadding, 0);
+            entry.transform.FindChild("EntryName").GetComponent<TextMeshProUGUI>().text = monster.Name;
+            entry.transform.FindChild("HPAmount").GetComponent<TextMeshProUGUI>().text = monster.CurrentHp + "/" + monster.MaxHp;
+            var hpFillAmount = (float)monster.CurrentHp / (float)monster.MaxHp;
+            Debug.Log("Calculated fill amount = " + hpFillAmount);
+            entry.transform.FindChild("HealthBar").GetComponent<Image>().fillAmount = hpFillAmount;
+            Debug.Log("HP fill updated. Current value = " + entry.transform.FindChild("HealthBar").GetComponent<Image>().fillAmount);
+            var statText = entry.transform.FindChild("Stats").GetComponent<TextMeshProUGUI>().text;
+            entry.transform.FindChild("Stats").GetComponent<TextMeshProUGUI>().text = String.Format(statText, monster.Attack, monster.Defense, monster.Speed);
         }
+
+        teamPane.gameObject.SetActive(false);
+        var button = hamburgerMenu.GetComponentInChildren<Button>();
+        button.onClick.AddListener(OnHamburgerClick);
     }
 
     public MonsterInteractionsMenu ShowMonsterInteractionsMenu(MapMonster monster)
@@ -53,5 +73,11 @@ public class UiController : MonoBehaviour {
     {
         monsterInteractionsMenu.monster = null;
         monsterInteractionsMenu.gameObject.SetActive(false);
+    }
+
+    void OnHamburgerClick()
+    {
+        var teamPane = hamburgerMenu.transform.FindChild("TeamPane");
+        teamPane.gameObject.SetActive(!teamPane.gameObject.activeSelf);
     }
 }
