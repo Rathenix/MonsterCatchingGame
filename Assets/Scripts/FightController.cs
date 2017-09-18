9b6d23b778b5b8b3922cdb2be3bee825cae97482
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-09-17 17:00:57 -0700

    Adjusted screen size for all scenes
    Should fit on iPhone 6 exactly right now. Made fight scene more visually
    friendly and you can tap to go back tot he map. Need to look into Canvas
    scaling for scaling UI elements to changing screen sizes.

--- Assets/Scripts/FightController.cs
+++ Assets/Scripts/FightController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FightController : MonoBehaviour {
 
     //public GameObject[] FightMonsters;
-
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Map");
+        }
+    }
 
 }
